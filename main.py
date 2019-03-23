b475dd11f8a6c6c517beecc4fddd699198d6bc53
Nathan Hartland <nathan@qlouder.com>
2019-03-23 18:41:49 +0100

    Add skeleton css

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-dash_app = dash.Dash()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash_app.server
 
-dash_app.layout = html.Div(children=[
+dash_app.layout = html.Div(className="container", children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
