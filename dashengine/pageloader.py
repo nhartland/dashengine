ffa799d35291ed5a659e573ca0d9c036fed5d60d
Nathan Hartland <nathan@qlouder.com>
2019-04-28 12:25:38 +0200

    Navigation bar work

---
+++ dashengine/pageloader.py
@@ -0,0 +1,23 @@
+""" Pageloader Module
+    Provides functionality for loading all the pages implemented in the project.
+"""
+# System
+import pkgutil
+import importlib
+import logging
+
+
+def page_loader(roots: list) -> dict:
+    """ Reads page modules from subdirectories specified in the `roots` list,
+    and returns them in a dictionary keyed by module.ROUTE. """
+    page_dict = {}
+    for root in roots:
+        for importer, package_name, _ in pkgutil.iter_modules([root]):
+            full_package_name = '%s.%s' % (root, package_name)
+            module = importlib.import_module(full_package_name)
+            route = module.ROUTE
+            logging.info(f'Page module \"{package_name}\" loaded at route \"{route}\"')
+            page_dict[route] = module
+    return page_dict
+
+
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import logging
 # Dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 # Local project
 from dashengine.dashapp import dashapp
-import dashengine.navigation as navigation
+import dashengine.bigquery as bigquery
+import dashengine.pageloader as pageloader
 
 
 # Setup logging level
@@ -16,8 +18,7 @@ logging.basicConfig(level=logging.DEBUG)
 app = dashapp.server
 
 # Read page modules
-ALL_PAGES = navigation.page_loader(["pages", "stdpages"])
-NAV_BAR = navigation.navigation_bar(ALL_PAGES)
+ALL_PAGES = pageloader.page_loader(["pages", "stdpages"])
 
 dashapp.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -27,10 +28,29 @@ dashapp.layout = html.Div([
 @dashapp.callback(Output('page-content', 'children'),
                   [Input('url', 'pathname')])
 def display_page(pathname: str) -> html.Div:
+    menu_opts = [dbc.DropdownMenuItem(mod.LINKNAME, href=mod.ROUTE) for mod in ALL_PAGES.values()]
     if pathname in ALL_PAGES:
-        return [NAV_BAR, ALL_PAGES[pathname].layout()]
+        return [ dbc.NavbarSimple(  # First build the navigation bar
+                 children=[
+                     dbc.NavbarBrand(f"{bigquery.PROJECT_ID}"),
+                     dbc.DropdownMenu(
+                         nav=True,
+                         in_navbar=True,
+                         label="Navigation",
+                         children=menu_opts
+                     ),
+                 ],
+                 brand=f"{ALL_PAGES[pathname].LINKNAME}",
+                 brand_href="/",
+                 sticky="top",
+                 ),                 # Secondly render the page layout
+                 ALL_PAGES[pathname].layout()]
     else:
         return '404'
+
+
+def navigation_bar(pathname) -> dbc.NavbarSimple:
+    """ Builds the navigation bar for the dashboards. """
 
 
 if __name__ == '__main__':
