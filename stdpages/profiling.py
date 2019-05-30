15f622f9840503dab57eea03028025cdb80131cf
Nathan Hartland <nathan@qlouder.com>
2019-05-30 19:03:44 +0200

    Neater profiling page:

--- stdpages/profiling.py
+++ stdpages/profiling.py
@@ -13,99 +13,51 @@ LINKNAME = "Profiling"
 TITLE = "Cached Query Profiler"
 
 
-def _query_billed_bytes_graph() -> dcc.Graph:
-    """ Generates a graph showing the cost in bytes-billed of the cached datasets. """
-    query_ids = []
-    query_bytes_billed = []
-    for query in bigquery.fetch_cached_queries():
-        query_ids.append(query.source.name)
-        query_bytes_billed.append(query.bytes_billed)
+def _query_profile_chart(gid: str, title: str, ylabel: str,
+                         xvalues: list, yvalues: list)  -> dcc.Graph:
+    """ Generates a bar chart showing a performance metric upon the cached datasets.
 
+    Args:
+        gid (str): A dash string identifier for the chart.
+        title (str): The title of the generated chart.
+        ylabel (str): The y-axis label of the generated chart.
+        xvalues (list): A list of the x-values in the bar chart.
+        yvalues (list): A list of the y-values in the bar chart.
+
+    Returns:
+        (dcc.Graph): A generated bar chart based upon the input arguments.
+    """
     return dcc.Graph(
-        id='profiler-billed-bytes',
+        id=gid,
         figure={
             'data': [
-                {'x': query_ids, 'y': query_bytes_billed, 'type': 'bar'}
+                {'x': xvalues, 'y': yvalues, 'type': 'bar'}
             ],
             'layout': {
-                'title': 'Query Bytes Billed',
-                'yaxis': { 'title': 'Bytes Billed'}
-            }
-        }
-    )
-
-
-def _query_processed_bytes_graph() -> dcc.Graph:
-    """ Generates a graph showing the processed bytes of the cached datasets. """
-    query_ids = []
-    query_bytes_processed = []
-    for query in bigquery.fetch_cached_queries():
-        query_ids.append(query.source.name)
-        query_bytes_processed.append(query.bytes_billed)
-
-    return dcc.Graph(
-        id='profiler-processed-bytes',
-        figure={
-            'data': [
-                {'x': query_ids, 'y': query_bytes_processed, 'type': 'bar'}
-            ],
-            'layout': {
-                'title': 'Query Bytes Processed',
-                'yaxis': { 'title': 'Bytes Processed'}
-            }
-        }
-    )
-
-
-def _query_timing_graph() -> dcc.Graph:
-    """ Generates a graph showing the timings of the cached datasets. """
-    query_ids = []
-    query_times = []
-    for query in bigquery.fetch_cached_queries():
-        query_ids.append(query.source.name)
-        query_times.append(query.duration)
-
-    return dcc.Graph(
-        id='profiler-time-performance',
-        figure={
-            'data': [
-                {'x': query_ids, 'y': query_times, 'type': 'bar'}
-            ],
-            'layout': {
-                'title': 'Query Cost (Time)',
-                'yaxis': { 'title': 'Query duration (s)'}
-            }
-        }
-    )
-
-
-def _query_memory_graph() -> dcc.Graph:
-    """ Generates a graph showing the memory (MB) usage of the cached datasets. """
-    query_ids = []
-    query_memory = []
-    for query in bigquery.fetch_cached_queries():
-        query_ids.append(query.source.name)
-        memory = query.result.memory_usage(index=True, deep=True).sum()
-        query_memory.append(memory/1E6)
-
-    return dcc.Graph(
-        id='profiler-memory-performance',
-        figure={
-            'data': [
-                {'x': query_ids, 'y': query_memory, 'type': 'bar'}
-            ],
-            'layout': {
-                'title': 'Query Cost (Memory)',
-                'yaxis': { 'title': 'Memory use (MB)'}
+                'title': title,
+                'yaxis': { 'title': ylabel}
             }
         }
     )
 
 
 def layout() -> html.Div:
+    """ Generates the layout for the query profiling page. """
+    # Compute performance metrics
+    queries = bigquery.fetch_cached_queries()
+    query_ids             = [query.source.name     for query in queries]
+    query_duration        = [query.duration        for query in queries]
+    query_bytes_processed = [query.bytes_processed for query in queries]
+    query_bytes_billed    = [query.bytes_billed    for query in queries]
+    query_memory          = [query.memory_usage()  for query in queries]
+
     return html.Div(className="container", children=[
-        _query_timing_graph(),
-        _query_memory_graph(),
-        _query_processed_bytes_graph(),
-        _query_billed_bytes_graph()
+        _query_profile_chart('profiler-memory-performance', 'Memory Usage', 'Memory use (MB)',
+                             query_ids, query_memory),
+        _query_profile_chart('profiler-time-performance', 'Query Duration', 'Duration (s)',
+                             query_ids, query_duration),
+        _query_profile_chart('profiler-bytes-processed', 'Processed Bytes ', 'Processed Bytes',
+                             query_ids, query_bytes_processed),
+        _query_profile_chart('profiler-bytes-billed', 'Billed Bytes', 'Billed Bytes',
+                             query_ids, query_bytes_billed)
     ])
