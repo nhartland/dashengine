ddd77c2a282be75b201d32033bfea5cf13763d25
Nathan Hartland <nathan@qlouder.com>
2019-06-04 00:36:51 +0200

    Tidying navigation bar

--- main.py
+++ main.py
@@ -24,35 +24,35 @@ dashapp.layout = html.Div([
     html.Div(id='page-content')
 ])
 
-@dashapp.callback(Output('page-content', 'children'),
-                  [Input('url', 'pathname')])
-def display_page(pathname: str) -> html.Div:
-    menu_opts = [dbc.DropdownMenuItem(mod.LINKNAME,
-                                      href=mod.ROUTE,
-                                      active=(pathname == mod.ROUTE))
-                                      for mod in ALL_PAGES.values()]
-    if pathname in ALL_PAGES:
-        return [ dbc.NavbarSimple(  # First build the navigation bar
-                 children=[
-                     #  dbc.NavbarBrand(f"{bigquery.PROJECT_ID}"), # Displays the project ID in the NavBar
-                     dbc.DropdownMenu(
-                         nav=True,
-                         in_navbar=True,
-                         label="Navigation",
-                         children=menu_opts
-                     ),
-                 ],
-                 brand=f"{ALL_PAGES[pathname].TITLE}",
-                 brand_href="/",
-                 sticky="top",
-                 ),                 # Secondly render the page layout
-                 ALL_PAGES[pathname].layout()]
-    else:
-        return '404'
-
 
 def navigation_bar(pathname) -> dbc.NavbarSimple:
     """ Builds the navigation bar for the dashboards. """
+    menu_opts = [dbc.DropdownMenuItem(mod.LINKNAME,
+                                      href=mod.ROUTE,
+                                      active=(pathname == mod.ROUTE))
+                 for mod in ALL_PAGES.values()]
+    return dbc.NavbarSimple(children=[
+                            # Uncomment to display the project ID in the NavBar
+                            #  dbc.NavbarBrand(f"{bigquery.PROJECT_ID}"),
+                            dbc.DropdownMenu(
+                                nav=True,
+                                in_navbar=True,
+                                label="Navigation",
+                                children=menu_opts)],
+                            brand=f"{ALL_PAGES[pathname].TITLE}",
+                            brand_href="/",
+                            sticky="top",
+                            )
+
+
+@dashapp.callback(Output('page-content', 'children'),
+                  [Input('url', 'pathname')])
+def display_page(pathname: str) -> html.Div:
+    if pathname in ALL_PAGES:
+        return [ navigation_bar(pathname),
+                 ALL_PAGES[pathname].layout()]
+    else:
+        return '404'
 
 
 if __name__ == '__main__':
