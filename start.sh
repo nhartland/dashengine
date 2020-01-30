bc06e52bab1fce53d84bd66d36f60f16d5c0ee56
Nathan Hartland <nathan@qlouder.com>
2020-01-30 23:44:49 +0100

    Moving configuration into demo folder

--- start.sh
+++ start.sh
@@ -3,17 +3,21 @@
 WORKDIR=/app
 DEMODIR=/app/demo
 
+# Check for configuration file, if it doesn't exist, use the demo
+if [ ! -f $WORKDIR/config.yaml ]; then
+  echo "No configuration file found, using demo configuration"
+  ln -s $DEMODIR/config.yaml $WORKDIR/config.yaml
+fi
+
 # Check for queries folder, if it doesn't exist, use the demo
 if [ ! -d $WORKDIR/queries ]; then
   echo "No queries directory found, using demo queries"
-  echo $DEMODIR/queries $WORKDIR/queries
   ln -s $DEMODIR/queries $WORKDIR/queries
 fi
 
 # Check for pages folder, if it doesn't exist, use the demo
 if [ ! -d $WORKDIR/pages ]; then
   echo "No pages directory found, using demo pages"
-  echo $DEMODIR/pages $WORKDIR/pages
   ln -s $DEMODIR/pages $WORKDIR/pages
 fi
 
