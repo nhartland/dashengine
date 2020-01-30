09f2130bf27f4588bed5b322056b3d87239bbbf3
Nathan Hartland <nathan@qlouder.com>
2020-01-30 23:36:31 +0100

    Debugging

--- start.sh
+++ start.sh
@@ -5,16 +5,16 @@ DEMODIR=/app/demo
 
 # Check for queries folder, if it doesn't exist, use the demo
 if [ ! -d $WORKDIR/queries ]; then
-  echo "No \`queries\' directory found, using demo queries"
-  echo $DEMODIR/queries
-  ls $DEMODIR/queries
-  cp -r $DEMODIR/queries $WORKDIR/queries
+  echo "No queries directory found, using demo queries"
+  echo $DEMODIR/queries $WORKDIR/queries
+  ln -s $DEMODIR/queries $WORKDIR/queries
 fi
 
 # Check for pages folder, if it doesn't exist, use the demo
 if [ ! -d $WORKDIR/pages ]; then
-  echo "No \`pages\' directory found, using demo pages"
-  cp -r $DEMODIR/pages $WORKDIR/pages.yaml
+  echo "No pages directory found, using demo pages"
+  echo $DEMODIR/pages $WORKDIR/pages
+  ln -s $DEMODIR/pages $WORKDIR/pages
 fi
 
 exec gunicorn --worker-tmp-dir=/dev/shm --workers=2 --threads=4 --worker-class=gthread main:app
